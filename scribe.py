727e3624c36834167a659283ac6140eeee433024
Charles-Éric <cbourget@mapgears.com>
2013-06-20 11:35:01 -0400

    Added significative error messages

--- scribe.py
+++ scribe.py
@@ -3,7 +3,7 @@
 
 import json
 import re
-import getopt, sys
+import getopt, sys, traceback
 import os.path
 import codecs
 
@@ -71,8 +71,6 @@ def parseList(d, data, scales, files, indentation, close=True):
                         parseScale(i, item[i], files, minScale, maxScale, indentation)
                     else:
                         parseDict(item, scales, files, indentation)
-            else:
-                print "Erreur: JSON mal formé"
 
     if (closeTag(data) == True and isScale(d) == False and isScaleList(data) == False and close == True):
         indentation = substractIndentation(indentation, INDENTATION)
@@ -210,7 +208,7 @@ def addIndentation(string, n):
     return string
 
 def substractIndentation(string, n):
-    n = -n;
+    n = -n
     return string[:n]
 
 def write(string, scales, files, indentation):
@@ -253,7 +251,7 @@ def closeFiles(files):
 
 def jsonToMap(content, outputDirectory, mapName, clean):
     global MAP, LAYERS, VAR, SCALES
-    data = json.loads(content);
+    data = json.loads(content)
 
     MAP = data["MAP"]
     LAYERS = data["LAYERS"]
@@ -289,7 +287,9 @@ def string2json(string):
     t = re.sub(r"/\*.*?\t*?\*/", "", t, flags=re.DOTALL)
     #Find and replace the comments preceded by ##
     comments = re.findall(r"\#\#.*", t, flags=0)
-    t = re.sub(r"\#\#.*", "VOID:FLAGCOMMENT\n", t, flags=0)   
+    t = re.sub(r"\#\#.*", "VOID:FLAGCOMMENT\n", t, flags=0)
+    #Remove tabs
+    t = re.sub(r"\t", "", t) 
     #Replace @ with VARIABLE:
     t = re.sub(r"@", "VARIABLE:", t)
     #Find and replace the text between {{ and }} (useful for blocks like PROJECTION, METADATA, PATTERN etc.)
@@ -315,11 +315,11 @@ def string2json(string):
     t = re.sub(r"\s(?=\w)", "{\"", t)
     #Replace line break with \""},\n
     t = re.sub(r"\"[\s\n]+", "\\\"\"},\n", t)
-    #Replace spaces preceded by alphanumeric characters or _ with "},\n
+    #Replace spaces preceded by alphabetic characters or _ with "},\n
     t = re.sub(r"(?<=[a-zA-Z_])\s", "\"},\n", t)    
     #Replace , followed by a ine break and ] with ]
     t = re.sub(r",\s+\n*\]", "]", t)    
-    #Replace ] not preceded by and alphanueric chracter with ]}
+    #Replace ] not preceded by and alphanumeric chracter with ]}
     t = re.sub(r"(?<!\w)\]", "]}", t) 
     #Substitute the FLAGVALUE with the corresponding values
     for i in range (0, len(values)):
@@ -344,12 +344,157 @@ def string2json(string):
     t = re.sub(r"\}\n*\s*\]\},\{\"MAP\"", "}],\"MAP\"", t)
     t = re.sub(r"\}\n*\s*\]\},\{\"VARIABLES\"", "}],\"VARIABLES\"", t)
     t = re.sub(r"^\s*\n*{\"", "", t)
-    #Replace the - between scales values with :
-    #t = re.sub(r"(?<=[0-9])-(?=[0-9])", ":", t)
     #Add {" before scale levels tag
     t = re.sub(r"\s(?=([0-9]|[0-9]{2})+\":)", "{\"", t)
     #Add {" at the beginning of the string and return a valid JSON string
     return ("{\"" + t)
+
+def validateInput(scales, variables, map, groups):
+    items = [scales, variables, map] + groups
+    correct = True
+    for item in items:
+        (errors, line_errors) = validateSyntax(content=item["content"], name=item["name"], variables=variables["content"])
+        if len(errors) > 0 or len(line_errors) > 0:
+            correct = False
+
+        for error in errors:
+            print >> sys.stderr, error
+
+        for line in sorted(line_errors.iterkeys()):
+            for error in line_errors[line]:
+                print >> sys.stderr, error["text"]
+                print >> sys.stderr, error["value"]
+
+    return correct
+
+def validateSyntax(content, name, variables=None):
+    lines = content.split("\n")
+    line_errors = {}
+    errors = []
+
+    #Remove the comments preceded by //
+    uncommented_string = re.sub(r"//.*", "", content)
+    #Remove the comments between /* and */
+    uncommented_string = re.sub(r"/\*.*?\t*?\*/", "", uncommented_string, flags=re.DOTALL)
+    #Find and replace the comments preceded by ##
+    uncommented_string = re.sub(r"\#\#.*", "", uncommented_string, flags=0)
+    #Remove the the plain text between {{}}
+    no_plain_text_string = re.sub(r"\{\{.*?\t*?\}\}", "{}", uncommented_string, flags=re.DOTALL)
+
+    #Unmatching brackets
+    test_string = re.sub(r"(?<=:)\s*(\"|\'|\/)+.*(\"|\'|\/)+", "FLAG:", uncommented_string)
+    if not(matchingBrackets(test_string)):
+        errors.append("Error : Brackets mismatch in file " + name)
+
+    if len(errors) == 0:    
+        #Missing ":"
+        #Remove the values preceded with :
+        test_string = re.sub(r"(?<=:).+", "FLAG:", no_plain_text_string)
+
+        missing_dots = re.findall(r"([\w\-]+\s+(?!\:)[\w\"\'\[\/@\-\.#=]{1}.*?\n)+?", test_string)
+        for value in list(set(missing_dots)):
+            value = value.strip()
+            for index, line in enumerate(lines):
+                line = line.strip()
+                if re.search(value, line):
+                    str_line_number = str(index + 1)
+                    if not str_line_number in line_errors:
+                        line_errors[str_line_number] = []
+
+                    line_errors[str_line_number].append({
+                        "text": "Error : Missing ':' in file " + name + " on line " + str_line_number + " :",
+                        "value": line        
+                    })
+
+        #Unexpected ":"
+        unexpected_dots = re.findall(r"(\:\s*\{|\{\s*\:)", no_plain_text_string)
+        unexpected_dots += re.findall(r"\w+\s*\:\s*\n+\s*\{", no_plain_text_string)
+        for value in list(set(unexpected_dots)):
+            value = value.strip()
+            for index, line in enumerate(lines):
+                line = line.strip()
+                if re.search(value, line):
+                    str_line_number = str(index + 1)
+                    if not str_line_number in line_errors:
+                        line_errors[str_line_number] = []
+
+                    line_errors[str_line_number].append({
+                        "text": "Error : Unexpected ':' in file " + name + " on line " + str_line_number + " :",
+                        "value": line        
+                    })
+
+        unexpected_dots2 = re.findall(r"\w+\s*\:\s*\n+\s*\{", no_plain_text_string)
+        for value in list(set(unexpected_dots2)):
+            value = value.split("\n")[0].strip()
+            for index, line in enumerate(lines):
+                line = line.strip()
+                if re.search(value, line):
+                    str_line_number = str(index + 1)
+                    if not str_line_number in line_errors:
+                        line_errors[str_line_number] = []
+
+                    line_errors[str_line_number].append({
+                        "text": "Error : Unexpected ':' in file " + name + " on line " + str_line_number + " :",
+                        "value": line        
+                    })
+        
+        #Unexpected plain text
+        test_string = re.sub(r"epsg\:", "epsg$", no_plain_text_string, flags=re.IGNORECASE)
+        no_plain_text_lines = test_string.split("\n")
+        for  no_plain_text_line in no_plain_text_lines:
+            no_plain_text_line = no_plain_text_line.strip()
+
+            if no_plain_text_line != "" and (not(re.search(r"(\:|\{|\}|@)", no_plain_text_line)) and re.search(r"^(\"|\'|[0-9]).+(\"|\'|[0-9])$", no_plain_text_line)):
+                for index, line in enumerate(lines):
+                    line = line.strip()
+                    no_plain_text_line = re.sub(r"epsg\$", "epsg:", no_plain_text_line, flags=re.IGNORECASE)
+                    if re.search(no_plain_text_line, line, flags=re.IGNORECASE):
+                        str_line_number = str(index + 1)
+                        if not str_line_number in line_errors:
+                            line_errors[str_line_number] = []
+
+                        line_errors[str_line_number].append({
+                            "text": "Error : Malformed text in file " + name + " on line " + str_line_number + \
+                            ". Consider using double brackets {{ }} :",
+                            "value": line        
+                        })
+        
+        unexpected_plain_text = re.findall(r"(\:\s*\{|\{\s*\:)", no_plain_text_string)
+
+        #Call to undefined variable
+        vars = re.findall(r"(?<=@).+", uncommented_string)
+        for var in vars:
+            var_re = re.compile("(?<!\w)" + var + "\s*(\:|\{)+")
+            #var_re = re.compile(var)
+            if not(var_re.search(variables)):
+                for index, line in enumerate(lines):
+                    line = line.strip()
+                    if re.search(var, line):
+                        str_line_number = str(index + 1)
+                        if not str_line_number in line_errors:
+                            line_errors[str_line_number] = []
+
+                        line_errors[str_line_number].append({
+                            "text": "Error : Call to undefined variable in file " + name + " on line " + str_line_number + " :",
+                            "value": line        
+                        })
+
+    return (errors, line_errors)
+
+def matchingBrackets(string):
+    iparens = iter('(){}[]<>')
+    parens = dict(zip(iparens, iparens))
+    closing = parens.values()
+    line = 1
+    stack = []
+    for c in string:
+        d = parens.get(c, None)
+        if d:
+            stack.append(d)
+        elif c in closing:
+            if not stack or c != stack.pop():
+                return False
+    return not stack
 
 def list2dict(ls):
     dc = {}
@@ -372,7 +517,7 @@ def main():
     try:                                
         opts, args = getopt.getopt(sys.argv[1:], "i:o:n:cf:t:", ["input", "output", "name", "clean", "file","tabulation"])
     except getopt.GetoptError as err:
-        print str(err)                      
+        print >> sys.stderr, str(err)                      
         sys.exit(2) 
                     
     for opt, arg in opts:
@@ -392,6 +537,7 @@ def main():
     if os.path.isfile(inputDirectory + "scales"):
         inputScalesFile = codecs.open(inputDirectory + "scales", encoding='utf-8')
         inputScalesContent = inputScalesFile.read()
+        scales = {"name": inputDirectory + "scales", "content": inputScalesContent}
         inputScalesFile.close()
     else:
         error += "File 'scales' not found.\n"
@@ -399,6 +545,7 @@ def main():
     if os.path.isfile(inputDirectory + "variables"):
         inputVariablesFile = codecs.open(inputDirectory + "variables", encoding='utf-8')
         inputVariablesContent = inputVariablesFile.read()
+        variables = {"name": inputDirectory + "variables", "content": inputVariablesContent}
         inputVariablesFile.close()
     else:
         error += "File 'variables' not found.\n"
@@ -406,21 +553,10 @@ def main():
     if os.path.isfile(inputDirectory + "map"):
         inputMapFile = codecs.open(inputDirectory + "map", encoding='utf-8')
         inputMapContent = inputMapFile.read()
+        map = {"name": inputDirectory + "map", "content": inputMapContent}
         inputMapFile.close()
     else:
         error += "File 'map' not found.\n"
-   
-    if os.path.isfile(inputDirectory + "map"):
-        inputMapFile = codecs.open(inputDirectory + "map", encoding='utf-8')
-        inputMapContent = inputMapFile.read()
-        inputMapFile.close()
-    else:
-        error += "File 'map' not found\n."
-
-    #if configFile[:1] == "/":
-    #    configFilePath = configFile
-    #else:
-    #    configFilePath = inputDirectory + configFile
 
     if os.path.isfile(configFile):
         inputConfigFile = codecs.open(configFile, encoding='utf8')
@@ -428,8 +564,9 @@ def main():
         inputConfigFile.close()
 
         jsonConfig = json.loads(string2json(inputConfigContent))
-        inputLayersContent = "LAYERS {\n";
+        inputLayersContent = "LAYERS {\n"
         groupFiles = [""] * (len(jsonConfig["ORDER"]) + 1)
+        groups= []
         
         for i in range(0, len(jsonConfig["ORDER"])):
             for j in jsonConfig["ORDER"][i]:
@@ -440,7 +577,7 @@ def main():
 
                 if (os.path.isfile(layerFilePath)):
                     if (int(j) > 0 and int(j) <= len(jsonConfig["ORDER"])):
-                        groupFiles[int(j)] = layerFilePath;
+                        groupFiles[int(j)] = layerFilePath
                     else:
                         error += "Index " + j + " out of bounds in config file.\n" 
                 else:
@@ -449,7 +586,9 @@ def main():
         for i in range(0, len(groupFiles)):
             if (os.path.isfile(groupFiles[i])):
                 inputLayerFile = codecs.open(groupFiles[i], encoding='utf8')
-                inputLayersContent += inputLayerFile.read() + "\n"
+                inputLayerContent = inputLayerFile.read()
+                inputLayersContent += inputLayerContent + "\n"
+                groups.append({"name": groupFiles[i], "content": inputLayerContent})
                 inputLayerFile.close()
 
         inputLayersContent += "\n}"
@@ -457,11 +596,18 @@ def main():
         error += "Config file not found.\n"
 
     if (error == ""):
-        jsonInput = inputScalesContent + "\n" + inputVariablesContent + "\n" + inputMapContent + "\n" + inputLayersContent;
-        jsonContent = string2json(jsonInput);
-        jsonToMap(jsonContent, outputDirectory, mapName, clean)
+        jsonInput = inputScalesContent + "\n" + inputVariablesContent + "\n" + inputMapContent + "\n" + inputLayersContent
+        correct = validateInput(scales=scales, variables=variables, map=map, groups=groups)
+        if correct == True:
+            jsonContent = string2json(jsonInput)
+            try:
+                jsonToMap(jsonContent, outputDirectory, mapName, clean)
+            except ValueError:
+                exc_traceback = sys.exc_info()[2]
+                print >> sys.stderr, "Uncatched syntax error :"
+                traceback.print_exc(exc_traceback)
     else:
-        print error
+        print >> sys.stderr, error
         sys.exit(2)
 
 if __name__ == "__main__":
